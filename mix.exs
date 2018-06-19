9a6e4e4333090821130b216a1b29a832ac1ee0e6
Eishun Kondoh <shun3382@gmail.com>
2018-06-20 08:53:48 +0900

    Add exdoc as dependency for hex publish

--- mix.exs
+++ mix.exs
@@ -4,8 +4,12 @@ defmodule Hyparview.MixProject do
   def project do
     [
       app: :hyparview,
+      name: "hyparview",
       version: "0.1.0",
       elixir: "~> 1.6",
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/shun159/hyparview",
       deps: deps(),
       aliases: aliases(),
       dialyzer: dialyzer()
@@ -22,7 +26,8 @@ defmodule Hyparview.MixProject do
   defp deps do
     [
       {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.14", only: :dev},
     ]
   end
 
@@ -36,6 +41,20 @@ defmodule Hyparview.MixProject do
       plt_add_deps: :app_tree,
       flags: [:unmatched_returns, :error_handling, :race_conditions],
       ignore_warnings: "dialyzer.ignore-warnings"
+    ]
+  end
+
+  defp description do
+    "HyParView Implementation In Elixir"
+  end
+
+  defp package do
+    [
+      name: "hyperview",
+      files: ["lib", "mix.exs", "README.md", "images"],
+      licenses: ["BSD 3-Clause"],
+      maintainers: ["Eishun Kondoh (shun159)"],
+      links: %{"GitHub" => "https://github.com/shun159/hyparview"}
     ]
   end
 end
