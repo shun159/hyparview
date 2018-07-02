915d07dafe2b087f8ccc4edbde2250f12acb7b42
Eishun Kondoh <shun3382@gmail.com>
2018-07-02 23:00:19 +0900

    version bump

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Hyparview.MixProject do
     [
       app: :hyparview,
       name: "hyparview",
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.6",
       description: description(),
       package: package(),
